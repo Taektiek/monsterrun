dec9aa795d67142c4ee961996c0e9bb24f8faaab
Taeke Roukema <roukema.taeke@gmail.com>
2020-07-28 11:47:20 +0200

    Fixed movement bug

--- Player.gd
+++ Player.gd
@@ -1,20 +1,22 @@
 extends Area2D
+signal hit
 
 export var speed = 400
 var screen_size
 
 func _ready():
+	hide()
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
 	var velocity = Vector2()
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
